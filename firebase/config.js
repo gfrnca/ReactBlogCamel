e1e6393c87983ba556be19b52da0c965ef390bf8
gfrnca <gabrielbfranca27@gmail.com>
2023-01-20 08:59:24 -0300

    Setup do projeto

---
+++ firebase/config.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCGAc8Byred5Bp_WbfgLu5gJTpQAnX5EnE",
+  authDomain: "camel-3c0d4.firebaseapp.com",
+  projectId: "camel-3c0d4",
+  storageBucket: "camel-3c0d4.appspot.com",
+  messagingSenderId: "396486730271",
+  appId: "1:396486730271:web:8310e3a8457341cbf4eb2a"
+};
+
+const app = initializeApp(firebaseConfig);
+
