ad8d8ab69de634c7f745d29fe8a9a4ca68acb94e
gfrnca <gabrielbfranca27@gmail.com>
2023-02-05 20:42:14 -0300

    Bloqueando páginas para usuários não autenticados

--- src/App.js
+++ src/App.js
@@ -1,64 +1,67 @@
 // General
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { onAuthStateChanged } from 'firebase/auth';
-  
-  // Hooks 
-  import { useState, useEffect } from 'react'
-  import { useAuthentication } from './hooks/useAuthentication';  
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+
+// Hooks
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
 
 // CSS
-import './App.css';
+import "./App.css";
 
-// Context 
-import { AuthProvider } from './context/AuthContext';
+// Context
+import { AuthProvider } from "./context/AuthContext";
 
 // Components
-import Navbar from './components/organisms/Navbar/Navbar';
+import Navbar from "./components/organisms/Navbar/Navbar";
 
 // Pages
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
-import CreatePost from './pages/CreatePost/CreatePost';
-import Dashboard from './pages/Dashboard/Dashboard';
-
-
-
-
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
-
-  const [user, setUser] = useState(undefined)
-  const {auth} = useAuthentication();
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       setUser(user);
-    })
-  }, [auth])
+    });
+  }, [auth]);
 
+  const loadingUser = user === undefined;
 
-  const loadingUser = user === undefined
-
-  if(loadingUser) {
-    return <p>Carregando...</p>
+  if (loadingUser) {
+    return <p>Carregando...</p>;
   }
 
   return (
     <div className="App">
       <AuthProvider value={{ user }}>
         <BrowserRouter>
-          <div className='container'>
+          <div className="container">
             <Navbar />
 
             <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/register' element={<Register />} />
-              <Route path='/posts/create' element={<CreatePost />} />
-              <Route path='/dashboard' element={<Dashboard />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route
+                path="/posts/create"
+                element={user ? <CreatePost /> : <Navigate to="/" />}
+              />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/register"
+                element={!user ? <Register /> : <Navigate to="/" />}
+              />
             </Routes>
           </div>
         </BrowserRouter>
